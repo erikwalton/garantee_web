aaf4e61014ab78b04584bedb5f3a6c3c1c5ad30d
Erik Walton <7532456+erikwalton@users.noreply.github.com>
2018-01-24 13:15:08 +0100

    Fixed Babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,11 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react']
                 }
             },
             {
